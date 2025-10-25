b1626f2a899e964d2f17ca2503386fc2d262ce7f
Yufan <yxiao986@connect.hkust-gz.edu.cn>
2025-10-25 21:10:37 +0800

    Parameting tuning: bs=256

--- assignment2/scripts/sft.sh
+++ assignment2/scripts/sft.sh
@@ -1,14 +1,14 @@
-export WANDB_API_KEY=""
+export WANDB_API_KEY="0f2cb2e82161bace177966302bdba87bff2a2f97"
 
 export WANDB_PROJECT="COMP4901B-Homework2"
-RUNNAME="HW2"
+RUNNAME="HW2_bs_256"
 MODELPATH="SmolLM2-135M"
 DATAPATH="smol-smoltalk-6k.json"
 MODEL_SIZE="0.6B"
 OUTPUTPATH="ckpt"
 DEVICES="0"  # e.g. 0,1,2,3
 NUM_GPUS=1
-TOTALBSZ=128
+TOTALBSZ=256
 BSZPERDEV=1
 GRADACC=$((TOTALBSZ / NUM_GPUS / BSZPERDEV))
 export CUDA_VISIBLE_DEVICES=${DEVICES}
@@ -31,7 +31,7 @@ python train_hw_parallel.py \
     --warmup_ratio 0.1 \
     --lr_scheduler_type "cosine" \
     --logging_steps 1 \
-    --do_eval False \
+    --do_eval True \
     --model_max_length 2048 \
     --lazy_preprocess True \
     --report_to "wandb" \
