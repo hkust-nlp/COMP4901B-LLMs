d287cac6f6543a22ad81185fb7cfb51b0fa23796
Yufan <yxiao986@connect.hkust-gz.edu.cn>
2025-10-26 13:49:17 +0800

    lr=5e-5

--- assignment2/scripts/sft.sh
+++ assignment2/scripts/sft.sh
@@ -1,7 +1,7 @@
 export WANDB_API_KEY="0f2cb2e82161bace177966302bdba87bff2a2f97"
 
 export WANDB_PROJECT="COMP4901B-Homework2"
-RUNNAME="HW2_3"
+RUNNAME="HW2_4"
 MODELPATH="SmolLM2-135M"
 DATAPATH="smol-smoltalk-6k.json"
 MODEL_SIZE="0.6B"
@@ -27,7 +27,7 @@ python train_hw_parallel.py \
     --save_strategy "steps" \
     --save_steps 5 \
     --save_total_limit 2 \
-    --learning_rate 1e-4 \
+    --learning_rate 5e-5 \
     --warmup_ratio 0.1 \
     --lr_scheduler_type "cosine" \
     --logging_steps 1 \
