fd6f5a4c95aaa8fc29c9438a788a02aa307c8acf
Yufan <yxiao986@connect.hkust-gz.edu.cn>
2025-10-28 22:06:06 +0800

    batchsize=64, maxlen=2368

--- assignment2/scripts/sft.sh
+++ assignment2/scripts/sft.sh
@@ -8,7 +8,7 @@ MODEL_SIZE="0.6B"
 OUTPUTPATH="ckpt"
 DEVICES="0"  # e.g. 0,1,2,3
 NUM_GPUS=1
-TOTALBSZ=256
+TOTALBSZ=64
 BSZPERDEV=1
 GRADACC=$((TOTALBSZ / NUM_GPUS / BSZPERDEV))
 export CUDA_VISIBLE_DEVICES=${DEVICES}
@@ -19,7 +19,7 @@ python train_hw_parallel.py \
     --model_name_or_path ${MODELPATH} \
     --data_path ${DATAPATH} \
     --output_dir ${OUTPUTPATH}/${RUNNAME} \
-    --num_train_epochs 2 \
+    --num_train_epochs 1 \
     --per_device_train_batch_size ${BSZPERDEV} \
     --per_device_eval_batch_size 1 \
     --gradient_accumulation_steps ${GRADACC} \
@@ -32,7 +32,7 @@ python train_hw_parallel.py \
     --lr_scheduler_type "cosine" \
     --logging_steps 1 \
     --do_eval True \
-    --model_max_length 2432 \
+    --model_max_length 2368 \
     --lazy_preprocess True \
     --report_to "wandb" \
     --run_name ${RUNNAME} \
